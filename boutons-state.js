c0e4618124b359996f5c1214725ae12b2b3a83ac
Flrnmll3r <113695742+Flrnmll3r@users.noreply.github.com>
2023-01-20 10:55:51 +0100

    Tentative + Padding
    Tentatives soldées par un échec à l'intégration du State button et du menu burger.
    
    Raccordement effectué entre les sections: Personnage/Pouvoir/Map

--- boutons-state.js
+++ boutons-state.js
@@ -1,11 +1,18 @@
-const square = document.querySelector(".Boutons");
+const boutons = document.querySelector(".Boutons_1_Survive");
 
-var boutonsImage
-boutonsImage = " ";
+var changeImage;
 
-boutons.addEventListener("click", function () {}
-console.log("Testing");
+changeImage = "";
 
-if (boutonsImage == " ") {
+console.log("Image");
 
-}
+boutons.addEventListener("click", function () {
+  if (changeImage == "Boutons_1_Survive") {
+    boutons.classList.add("Boutons_2_Survive");
+    changeImage = "Boutons_2_Survive";
+  } else if (changeImage == "Boutons_2_Survive") {
+    boutons.classList.add("Boutons_1_Survive");
+    boutons.classList.remove("Boutons_2_Survive");
+    changeImage = "Boutons_1_Survive";
+  }
+});
